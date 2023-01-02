b457269fff1f106b50d6ce0352eff303b1888fab
kalkidan-y <kalkidanyibeltal55@gmail.com>
2023-01-02 11:00:53 -0800

    well practice

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+
+print(str[39:67] + str[-24:-17] + str[:6])
+
